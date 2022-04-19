1b415843b40ead89b31f148f4ede118bfbf345ff
ionydeodo <jhony.atehortua@alternova.io>
2022-04-19 07:05:44 -0500

    base

---
+++ Phothon/Assets/Scripts/ConnectCtrl.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public enum RegionsCodes
+{
+    AUTO,
+    CAE,
+    EU,
+    US,
+    USW
+}
+public class ConnectCtrl : MonoBehaviourPunCallbacks
+{
+
+    string gameVersion = "1";
+
+
+    [SerializeField]
+    private string regionCode = null;
+
+    [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    public void SetRegion(int index)
+    {
+        RegionsCodes region = (RegionsCodes)index;
+
+        if(region == RegionsCodes.AUTO)
+        {
+            regionCode = null;
+        }
+        else
+        {
+            regionCode = region.ToString();
+        }
+
+        Debug.Log("Region: " + regionCode);
+        PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = regionCode;
+    }
+
+    #region MonoBehaviourPunCallbacks Callbacks
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName +" Now  is in a room.");
+        SceneManager.LoadScene("game2");
+    }
+
+    #endregion
+
+
+}
---
+++ Phothon/Assets/Scripts/GameCtrl.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPrefab;
+    [SerializeField]
+    private Transform Player1SpawnPoint;
+    [SerializeField]
+    private Transform Player2SpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        if(playerPrefab == null)
+        {
+            Debug.LogError("Falta la referencia al player prefab");
+        }
+        else
+        {
+            Transform spawnPoint = (PhotonNetwork.IsMasterClient) ? Player1SpawnPoint : Player2SpawnPoint;
+            PhotonNetwork.Instantiate(this.playerPrefab.name, spawnPoint.position, Quaternion.identity, 0);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
